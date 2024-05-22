deae1af72ad88869a822f179f5567f48bf16742c
Rowan <104235853+annastratton@users.noreply.github.com>
2024-05-22 17:12:46 -0500

    adding components and style changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';  // Assume you have a FooterComponent
+import { HomePageComponent } from './pages/home-page/home-page.component';  // Assume you have a HomeComponent
+import { AboutPageComponent } from './pages/about-page/about-page.component';  // Assume you have an AboutComponent
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'about', component: AboutPageComponent },
+  // Add more routes as needed
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    AboutPageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes)  // Configure routes
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/layout/footer/footer.component.html
@@ -0,0 +1,3 @@
+<footer>
+  <p>&copy; 2024 My Portfolio. All rights reserved.</p>
+</footer>
